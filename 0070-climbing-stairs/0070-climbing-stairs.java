8ecf255b97931a7e8d7c2f6731d51103b26a5bc7
shivamsandy <shivamsandy3183@gmail.com>
2026-07-30 00:12:28 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -4,8 +4,9 @@ class Solution {
         Arrays.fill(dp,-1);
         
         return climb_stair(n,dp);
+
     }
-     public   int climb_stair(int n ,int dp[]){
+     public   int climb_stair(int n ,int dp[]){  //  using memoization
         if(n<=2) return n;
 
         if(dp[n] !=-1){
@@ -16,4 +17,11 @@ class Solution {
 
         return dp[n];
     }
+    // public int climb_rec(int n ){ 
+
+    //     // 2^n TLE IN LEETCODE
+    //     if(n<=2) return n;
+
+    //     return climb_rec(n-1) + climb_rec(n-2);
+    // }
 }
