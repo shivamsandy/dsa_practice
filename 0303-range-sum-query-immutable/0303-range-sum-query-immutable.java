bbebc737b17245b17e8394ade4020d79fe8cfe44
shivamsandy <shivamsandy3183@gmail.com>
2026-08-05 05:43:18 +0530

    Time: 8 ms (70.79%), Space: 47.7 MB (62.91%) - LeetHub

---
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -0,0 +1,25 @@
+class NumArray {
+    int prefix[];
+    
+
+    public NumArray(int[] nums) {
+           prefix = new int[nums.length];
+        prefix[0] =nums[0];
+        for(int i =1;i<nums.length;i++){
+            prefix[i] = nums[i]+prefix[i-1];
+        }
+        
+    }
+    
+    public int sumRange(int left, int right) {
+
+         
+         return (left==0)? prefix[right] : prefix[right] -prefix[left-1];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
