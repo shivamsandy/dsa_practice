d5c29525974b04aa6855b2fc6fed5e8edf313417
shivamsandy <shivamsandy3183@gmail.com>
2026-08-22 02:44:52 +0530

    Time: 1 ms (27.85%), Space: 42.9 MB (84.33%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        String word[] = s.split(" ");
+        String lastword = word[word.length-1];
+        int length = lastword.length();
+        return length;
+        
+    }
+}
