5afc728d99760cdad1297303cfcf4c2922170f88
shivamsandy <shivamsandy3183@gmail.com>
2026-08-23 04:37:32 +0530

    Time: 3 ms (43.71%), Space: 44.1 MB (21.37%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.java
+++ 1122-relative-sort-array/1122-relative-sort-array.java
@@ -1,35 +1,38 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
 
-        TreeMap<Integer,Integer> map  =  new TreeMap<>();
-        int ans[] =  new int [arr1.length];
+        //  easy code 
 
-        for(int i =0;i<arr1.length;i++){
-            map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);
+        TreeMap<Integer, Integer> map = new TreeMap<>(); 
+        // bss yea dmg mai nahi aaya  CHII SASURRR...
+        
+
+        int ans[] = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            map.put(arr1[i], map.getOrDefault(arr1[i], 0) + 1);
         }
-        int y=0;
+        int y = 0;
 
-        for(int i=0;i<arr2.length;i++){
-             int freq  = map.get(arr2[i]);
-             map.remove(arr2[i]);
-             for(int j=0;j<freq;j++){
-                ans[y++] =arr2[i];
-             }
-            
+        for (int i = 0; i < arr2.length; i++) {
+            int freq = map.get(arr2[i]);
+            map.remove(arr2[i]);
+            for (int j = 0; j < freq; j++) {
+                ans[y++] = arr2[i];
+            }
+
         }
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
 
-    int key = entry.getKey();
-    int freq = entry.getValue();
+            int key = entry.getKey();
+            int freq = entry.getValue();
 
-    for (int j = 0; j < freq; j++) {
-        ans[y++] = key;
-    }
-}
+            for (int j = 0; j < freq; j++) {
+                ans[y++] = key;
+            }
+        }
 
         return ans;
-
-
 
     }
 }
