62236d2a321b2e4f5ef8c87f288903b88ce80c21
shivamsandy <shivamsandy3183@gmail.com>
2026-09-18 10:17:17 +0530

    Time: 1 ms (89.66%), Space: 43.8 MB (17.59%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.java
@@ -10,12 +10,16 @@ class Solution {
                 sum = sum + value * count_power;
                 count_power =  count_power*26;
 
-            }else{
-                sum = ch-'A'+1;
-                count_power++;
-            }
+            } 
         }
         return sum;
         
     }
+    //   int result = 0;
+    //     for (int i = 0; i < columnTitle.length(); i++) {
+
+    //         int value = columnTitle.charAt(i) - 'A' + 1;
+
+    //         result = result * 26 + value;
+    //     }
 }
